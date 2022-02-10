f149dd4a1026e69343e9fbc6251a048abf9acb38
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-02-10 18:44:10 +0200

    remove env

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,18 @@
+from django.db import router
 from django.urls import include, path
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+#register your view set
+from .routers import *
+
 
 
 urlpatterns = [
-    # path('', ),
+    path('', include(router.urls)),
+      path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+  
     
 ]
